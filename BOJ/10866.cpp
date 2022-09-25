84bc26b1ec3875fcb769afa07213ccd147cc1a0a
khunlee0508@gmail.com <khunlee0508@gmail.com>
2022-09-25 17:28:59 +0900

    commit

---
+++ BOJ/10866.cpp
@@ -0,0 +1,69 @@
+// 10866 덱
+#include <iostream>
+#include <deque>
+
+using namespace std;
+
+int main(void) {
+    int T, value;
+    string cmd;
+    deque<int> dq;
+
+    cin >> T;
+    while (T--) {
+        cin >> cmd;
+
+        if (cmd == "push_back") {
+            cin >> value;
+            dq.push_back(value);
+        }
+
+        else if (cmd == "push_front") {
+            cin >> value;
+            dq.push_front(value);
+        }
+
+        else if (cmd == "front") {
+            if (dq.empty())
+                cout << "-1\n";
+            else
+                cout << dq.front() << "\n";
+        }
+
+        else if (cmd == "back") {
+            if (dq.empty())
+                cout << "-1\n";
+            else
+                cout << dq.back() << "\n";
+        }
+
+        else if (cmd == "size") {
+            cout << dq.size() << "\n";
+        }
+
+        else if (cmd == "empty") {
+            if (dq.empty())
+                cout << "1\n";
+            else
+                cout << "0\n";
+        }
+        else if (cmd == "pop_front") {
+            if (dq.empty())
+                cout << "-1\n";
+            else {
+                cout << dq.front() << "\n";
+                dq.pop_front();
+            }
+        }
+        else if (cmd == "pop_back") {
+            if (dq.empty())
+                cout << "-1\n";
+            else {
+                cout << dq.back() << "\n";
+                dq.pop_back();
+            }
+        }
+    }
+    
+    return 0;
+}
