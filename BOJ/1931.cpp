0be4e39a778721e52982f67326959ba75b21f78f
khunlee0508@gmail.com <khunlee0508@gmail.com>
2022-10-06 21:59:22 +0900

    commits

---
+++ BOJ/1931.cpp
@@ -0,0 +1,44 @@
+// 1931 회의실 배정
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool comapre(pair<int, int> a, pair<int, int> b) {
+    if (a.second == b.second)
+        return a.first < b.first;
+    return a.second < b.second;
+}
+
+int solution(int n, vector<pair<int,int>> v) {
+    int answer = 1;
+
+    sort(v.begin(), v.end(), comapre);
+    int standard = v[0].second;
+
+    for (int i = 1; i < v.size(); ++i) {
+        if (standard <= v[i].first) {
+            answer++;
+            standard = v[i].second;
+        }
+    }
+    return answer;
+}
+
+int main(void) {
+    int n, answer;
+    vector<pair<int,int>> v;
+
+    cin >> n;
+    v = vector<pair<int,int>>(n, {0, 0});
+
+    for (int i = 0; i < n; ++i) 
+    {
+        cin >> v[i].first >> v[i].second;
+    }
+
+    answer = solution(n, v);
+    cout << answer;
+    return 0;
+}
