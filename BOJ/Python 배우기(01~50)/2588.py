0b7f386c944481512d0532c8b4d3fa442d534787
khunlee0508@gmail.com <khunlee0508@gmail.com>
2022-10-10 21:12:47 +0900

    commit

---
+++ BOJ/Python 배우기(01~50)/2588.py
@@ -0,0 +1,8 @@
+# 2588 곱셈 
+number1 = input()
+number2 = input()
+
+print(int(number1) * int(number2[2]))
+print(int(number1) * int(number2[1]))
+print(int(number1) * int(number2[0]))
+print(int(number1) * int(number2))
---
+++ BOJ/Python 배우기(01~50)/3046.py
@@ -0,0 +1,4 @@
+# 3046 R2
+R1, S = map(int, input().split())
+
+print(2 * S - R1)
