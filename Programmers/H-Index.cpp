9a42d41ebeabeb1a6b4354ac526ba6941e0f6963
khunlee0508@gmail.com <khunlee0508@gmail.com>
2022-10-20 21:14:11 +0900

    commit

--- Programmers/H-Index.cpp
+++ Programmers/H-Index.cpp
@@ -5,24 +5,20 @@
 
 using namespace std;
 
-int compare(int a, int b) {
+bool compare(int a, int b) {
     return a > b;
 }
 
 int solution(vector<int> citations) {
     int answer = 0;
-    int size = citations.size();
     
     sort(citations.begin(), citations.end(), compare);
     
-    for (int i = 0; i < size; ++i) {
-        if (citations[i] > i)
+    for (auto c: citations) {
+        if (c > answer)
             answer++;
         else
-        {
-            break;    
-        }
+            break;
     }
-    
     return answer;
 }
