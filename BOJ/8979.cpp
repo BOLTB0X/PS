9e03fbcd8133de526d7d7143afa8bd4cad2d9344
khunlee0508@gmail.com <khunlee0508@gmail.com>
2022-10-29 15:10:40 +0900

    commit

--- BOJ/8979.cpp
+++ BOJ/8979.cpp
@@ -1,13 +1,84 @@
+// 올림픽 8979
 #include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int board[1001][3];
+vector<vector<int>> board;
+
+/* 정렬 ver */
+struct medal {
+    int gold, silver, bronze;
+};
+
+bool compare(const medal &a, const medal &b) {
+    if (a.gold == b.gold) {
+        if (a.silver == b.silver)
+            return a.bronze > b.bronze;
+        return a.silver > b.silver;
+    }
+    return a.gold > b.gold;
+}
+
+int solution(int n, int k) {
+    int answer = 0;
+    int t_g, t_s, t_b;
+    vector<medal> v;
+
+    for (int i = 1; i <= n; ++i) {
+        if (i == k) {
+            t_g = board[k][0], t_s = board[k][1], t_b = board[k][2];
+        }
+
+        v.push_back({board[i][0], board[i][1], board[i][2]});
+    }
+
+    sort(v.begin(), v.end(), compare);
+
+    answer = 1;
+    for (auto m : v) {
+        if (m.gold == t_g && m.silver == t_s && m.bronze == t_b)
+            break;
+        
+        answer++;
+    }
+    
+    return answer;
+}
+/********************/
+
+/* 완전탐색 */
+int solution2(int n, int k) {
+    int answer = 1;
+
+    for (int i = 1; i <= n; ++i) {
+        if (board[k][0] < board[i][0]) {
+            answer++;
+        }
+
+        if (board[k][0] == board[i][0]) {
+            if (board[k][1] < board[i][1])
+                answer++;
+        }
+
+        if ((board[k][0] == board[i][0]) && (board[k][1] == board[i][1])) {
+            if (board[k][2] < board[i][2])
+                answer++;
+        }
+
+    }
+
+    return answer;
+}
+/*******************/
 
 int main(void) {
     int n, k, tmp, ans = 1;
 
     cin >> n >> k;
+    board = vector<vector<int>> (n + 1, vector<int> (3));
+
     for (int i = 0; i < n; ++i) {
         cin >> tmp;
 
@@ -16,19 +87,7 @@ int main(void) {
         }
     }
 
-    for (int i = 1; i <= n; ++i) {
-        if (board[k][0] < board[i][0])
-            ans++;
-
-        if (board[k][0] == board[i][0] && board[k][1] < board[i][1])
-            ans++;
-
-        if (board[k][0] == board[i][0] 
-        && board[k][1] == board[i][1]
-        && board[k][2] < board[i][2])
-            ans++;
-    }
-
-    cout << ans;
+    int ret = solution2(n, k);
+    cout << ret;
     return 0;
 }
