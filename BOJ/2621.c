f30b572b42236560cee22d2fc6e4e64857766042
khunlee0508@gmail.com <khunlee0508@gmail.com>
2022-10-29 14:35:43 +0900

    commit

--- BOJ/2621.c
+++ BOJ/2621.c
@@ -2,9 +2,14 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int compare(const void* a, const void* b) {
-	int n1 = *(int*)a;
-	int n2 = *(int*)b;
+char colors[5];
+int nums[5];
+bool check[9];
+
+int compare(const void *a, const void *b)
+{
+	int n1 = *(int *)a;
+	int n2 = *(int *)b;
 
 	if (n1 < n2)
 		return -1;
@@ -13,137 +18,131 @@ int compare(const void* a, const void* b) {
 	return 0;
 }
 
-void solution(void) {
-	char colors[5];
-	int nums[5];
+int solution(void)
+{
+	int answer = 0;
+	bool check[9] = {false, };
 
-	for (int i = 0; i < 5; ++i) 
-		scanf(" %c %d", &colors[i], &nums[i]);
-
+	// 정렬
 	qsort(nums, 5, sizeof(int), compare);
-	
-	bool check[9];
 
-	//1번 조건
+	// 1번 조건
 	check[0] = true;
-	
-	for (int i = 0; i < 4; ++i) {
-		if (colors[i] != colors[i + 1])
+	char c = colors[0];
+	for (int i = 1; i < 5; ++i) {
+		if (c != colors[i])
 			check[0] = false;
 	}
 
 	int tmp = nums[0];
-
-	if (check[0]) {
-		for (int i = 1; i < 5; ++i) {
-			if (++tmp != nums[i])
-				check[0] = false;
-		}
-	}
-	if (check[0]) {
-		printf("%d\n", 900 + nums[4]);
-		return;
+	for (int i = 1; i < 5; ++i) {
+		if (++tmp != nums[i])
+			check[0] = false;
 	}
 
-	//2번 조건
+	if (check[0]) {
+		answer = 900 + nums[4];
+		return answer;
+	}
+
+	// 2번 조건
 	tmp = 0;
 	check[1] = false;
-	
 	if (nums[1] == nums[4] || nums[0] == nums[3])
 		check[1] = true;
 
 	if (check[1]) {
-		printf("%d\n", 800 + nums[2]);
-		return;
+		answer = 800 + nums[2];
+		return answer;
 	}
 
-	//3번 조건
+	// 3번 조건
 	check[2] = false;
+	int max_v = nums[4], min_v = nums[0];
 	int cnt1 = 0, cnt2 = 0;
-	int max_val = nums[4];
-	int min_val = nums[0];
 
 	for (int i = 0; i < 5; ++i) {
-		if (nums[i] == max_val)
+		if (max_v == nums[i])
 			cnt1++;
-		if (nums[i] == min_val)
+		if (min_v == nums[i])
 			cnt2++;
 	}
 
 	if (cnt1 == 3 && cnt2 == 2) {
 		check[2] = true;
-		tmp = max_val * 10 + min_val;
+		tmp = max_v * 10 + min_v;
 	}
 	if (cnt1 == 2 && cnt2 == 3) {
 		check[2] = true;
-		tmp = min_val * 10 + max_val;
+		tmp = min_v * 10 + max_v;
 	}
 
 	if (check[2]) {
-		printf("%d\n", 700 + tmp);
-		return;
+		answer = tmp + 700;
+		return answer;
 	}
 
-	//4번조건
+	// 4번 조건
 	check[3] = true;
-
 	for (int i = 0; i < 5; ++i) {
 		if (colors[0] != colors[i])
 			check[3] = false;
 	}
-
-	if (check[3]) {
-		printf("%d\n", 600 + nums[4]);
-		return;
-	}
 	
-	//5번조건
+	if (check[3]) {
+		answer = 600 + nums[4];
+		return answer;
+	}
+
+	// 5번 조건
 	check[4] = true;
 	tmp = nums[0];
 	for (int i = 1; i < 5; ++i) {
-		if (++tmp != nums[i])
+		if (++tmp != nums[i]) {
 			check[4] = false;
+		}
 	}
 
 	if (check[4]) {
-		printf("%d\n", 500 + nums[4]);
-		return;
+		answer = 500 + nums[4];
+		return answer;
 	}
-	
-	//6번조건
-	check[5] = true;
-	tmp = nums[2];
+
+	// 6번 조건
 	int cnt = 0;
+	tmp = nums[2];
+	check[5] = true;
+
 	for (int i = 0; i < 5; ++i) {
-		if (tmp == nums[i])
+		if (nums[i] == tmp)
 			cnt++;
 	}
 
-	if (cnt != 3)
+	if (cnt != 3) {
 		check[5] = false;
+	}
 
 	if (check[5]) {
-		printf("%d\n", 400 + nums[2]);
-		return;
+		answer = 400 + nums[2];
+		return answer;
 	}
 
-	//7번 조건
+	// 7번 조건
 	check[6] = false;
 	tmp = 0;
-	//끝만 다른 경우
-	if ((nums[0] == nums[1]) && (nums[2] == nums[3]))
+	if (nums[0] == nums[1] && nums[2] == nums[3])
 		check[6] = true;
-	if ((nums[1] == nums[2]) && (nums[3] == nums[4]))
+	else if (nums[1] == nums[2] && nums[3] == nums[4])
 		check[6] = true;
-	if ((nums[0] == nums[1]) && (nums[3] == nums[4]))
+	else if (nums[0] == nums[1] && nums[3] == nums[4])
 		check[6] = true;
-
-	if (check[6]) {
-		printf("%d\n", 300 +nums[3] * 10 + nums[1]);
-		return;
-	}
 	
-	//8번조건
+	if (check[6]) {
+		answer = 300 + nums[3] * 10 + nums[1];
+		return answer;
+	}
+
+	// 8번 조건
 	check[7] = false;
 	int eq;
 	for (int i = 0; i < 4; ++i) {
@@ -154,16 +153,20 @@ void solution(void) {
 	}
 
 	if (check[7]) {
-		printf("%d\n", 200 + eq);
-		return;
+		answer = 200 + eq;
+		return answer;
 	}
 
-	//9번
-	printf("%d\n", 100 + nums[4]);
-	return;
+	// 9번 조건
+	answer = 100 + nums[4];
+	return answer;
 }
 
 int main(void) {
-	solution();
+	for (int i = 0; i < 5; ++i)
+		scanf(" %c %d", &colors[i], &nums[i]);
+
+	int ret = solution();
+	printf("%d", ret);
 	return 0;
 }
