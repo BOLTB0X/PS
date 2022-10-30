212f192fc953c4d974c3174b6e0e0fd9264d4105
khunlee0508@gmail.com <khunlee0508@gmail.com>
2022-10-30 14:24:08 +0900

    commit

--- BOJ/1012.c
+++ BOJ/1012.c
@@ -5,11 +5,12 @@
 int board[51][51] = {0, };
 int visited[51][51] = {0, };
 
-const int dy[4] = {-1,1,0,0};
-const int dx[4] = {0,0,-1,1};
+// 상하좌우
+const int dy[4] = {-1, 1, 0, 0};
+const int dx[4] = {0, 0, -1, 1};
 
 void DFS(int n, int m, int y, int x) {
-    for (int dir = 0; dir <4;++dir) {
+    for (int dir = 0; dir < 4; ++dir) {
         int ny = y + dy[dir];
         int nx = x + dx[dir];
 
@@ -21,7 +22,7 @@ void DFS(int n, int m, int y, int x) {
 
         if (board[ny][nx] == 0)
             continue;
-        
+
         visited[ny][nx] = 1;
         DFS(n, m, ny, nx);
     }
@@ -29,37 +30,40 @@ void DFS(int n, int m, int y, int x) {
     return;
 }
 
-int main(void)
-{
+int solution(int n, int m, int k) {
+    int answer = 0;
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < m; ++j) {
+            if (board[i][j] == 1 && visited[i][j] == 0) {
+                answer++;
+                visited[i][j] = 1;
+                DFS(n, m, i, j);
+            }
+        }
+    }
+
+    return answer;
+}
+
+int main(void) {
     int T;
     int n, m, k, y, x;
     scanf("%d", &T);
 
-    while (T--)
-    {
-        int ans = 0;
-
+    while (T--) {
         scanf("%d %d %d", &m, &n, &k);
         for (int i = 0; i < k; ++i) {
             scanf("%d %d", &x, &y);
             board[y][x] = 1;
         }
 
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                if (board[i][j] == 1 && visited[i][j] == 0) {
-                    ans++;
-                    visited[i][j] = 1;
-                    DFS(n, m, i, j);
-                }
-            }
-        }
-
-        printf("%d\n", ans);
+        int ret = solution(n, m, k);
+        printf("%d\n", ret);
 
         memset(board, 0, sizeof(board));
         memset(visited, 0, sizeof(visited));
     }
-    
+
     return 0;
 }
