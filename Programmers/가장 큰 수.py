7df35b311339ce6cc320f8188765d19f2c15274b
khunlee0508@gmail.com <khunlee0508@gmail.com>
2022-11-03 20:32:24 +0900

    commit

---
+++ Programmers/가장 큰 수.py
@@ -0,0 +1,15 @@
+def solution(numbers):
+    answer = ''
+    str_num = []
+    
+    for num in numbers:
+        str_num.append(str(num))
+    
+    str_num.sort(key = lambda n : n * 3, reverse = True)
+    
+    for s in str_num:
+        answer += s
+    
+    if answer[0] == '0':
+        answer = '0'
+    return answer
