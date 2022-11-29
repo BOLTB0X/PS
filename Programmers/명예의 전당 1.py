98ceab0fc2676a54170b2f799b22d9fd9189a809
BOLTB0x <khunlee0508@gmail.com>
2022-11-29 21:17:41 +0900

    commit

--- Programmers/명예의 전당 1.py
+++ Programmers/명예의 전당 1.py
@@ -1,20 +1,17 @@
-# 연습문제 level 1
 def solution(k, score):
     answer = []
-    rank = []
+    rank = [] # 명예전당 리스트
     
     for i in range(len(score)):
+        # 순위 비교
         if i + 1 > k:
-            # k순위 보다 클때마다
-            if score[i] > rank[0]:
+            # 진입 가능하면
+            if rank[0] < score[i]:
                 rank.append(score[i])
                 rank.remove(rank[0])
-        # rank의 길이가 k보다 작을 때
+        # 랭크 길이가 k가 되기 전에는+
         else:
             rank.append(score[i])
         rank.sort()
         answer.append(rank[0])
-            
     return answer
-
-print(solution(3, [10, 100, 20, 150, 1, 100, 200]))
