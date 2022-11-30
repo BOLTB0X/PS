2b1f428e92234cfb08f6b0143ecf42f6a4f70aea
BOLTB0x <khunlee0508@gmail.com>
2022-11-30 21:35:11 +0900

    commit

--- Programmers/햄버거 만들기.py
+++ Programmers/햄버거 만들기.py
@@ -1,11 +1,12 @@
-# 연습문제 level 1
 def solution(ingredient):
     answer = 0
     cooking = []
     
+    # 단순 구현
     for i in ingredient:
         cooking.append(i)
         
+        # 빵 야채 고기 빵 이순이 될때
         if len(cooking) >= 4:
             if cooking[len(cooking) - 4] == 1 and cooking[len(cooking) - 3] == 2 and cooking[len(cooking) - 2] == 3 and cooking[len(cooking) - 1] == 1:
                 answer += 1
