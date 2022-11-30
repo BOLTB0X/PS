2dbf9b7d7e917e5b1669dd0bb69c069e70c6f25a
BOLTB0x <khunlee0508@gmail.com>
2022-11-30 22:05:38 +0900

    commit

--- Programmers/기사단원의 무기.py
+++ Programmers/기사단원의 무기.py
@@ -2,18 +2,21 @@
 def solution(number, limit, power):
     answer = 0
     divisors = []
-
+    
     for num in range(1, number + 1):
-        cnt = 0
+        cnt = 0 # 갯수 초기화
+        # 약수 체크
+        # 시간 초과 방지
         for i in range(1, int(num**(1/2)) + 1):
             if (num % i == 0):
                 cnt += 1
+                # 시간 초과 방지
                 if ( (i**2) != num) : 
                     cnt += 1
         divisors.append(cnt);
-
+        
     for d in divisors:
-        if d > limit:
+        if d > limit: # 넘는다면 
             answer += power
         else:
             answer += d
