a8c26a398545773b226584c68f4363b41628894d
BOLTB0x <khunlee0508@gmail.com>
2022-12-29 22:52:11 +0900

    commit

--- Programmers/큰 수 만들기.cpp
+++ Programmers/큰 수 만들기.cpp
@@ -1,4 +1,3 @@
-// level 2
 #include <string>
 #include <vector>
 
@@ -7,17 +6,20 @@ using namespace std;
 string solution(string number, int k) {
     string answer = "";
     int size = number.length();
-    int max_idx = 0;
+    int max_idx = 0; // 비교용
     
+    // 변수 size 길이의 문자열안에 k만큼 써야함
     for (int i = 1; i <= size - k; ++i) {
-        int max_num = -1;
-        for (int j = max_idx; j < k + i; ++j) {
-            if (max_num < number[j] - '0') {
-                max_num = number[j] - '0';
-                max_idx = j;
-            }        
+        int max_number = -1; // 탐색 전 초기화
+        // max_idx부터 K만큼 큰 수 탐색
+        // 처음에는 0
+        for (int j = max_idx ; j < k + i; ++j) {
+            if (max_number < number[j] - '0') {
+                max_number = number[j] - '0';
+                max_idx = j; // 스티트 구분을 위해
+            }
         }
-        answer += (max_num + '0');
+        answer += (max_number + '0');
         max_idx++;
     }
     return answer;
